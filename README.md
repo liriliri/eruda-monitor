dca77cd9264d6a1582adf6483ba151a786966ce4
surunzi <surunzi@foxmail.com>
2024-07-20 10:15:03 +0800

    release: v1.0.2

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # eruda-monitor
 
+[![NPM version][npm-image]][npm-url]
+[![Build status][ci-image]][ci-url]
+[![License][license-image]][npm-url]
+
+[npm-image]: https://img.shields.io/npm/v/eruda-monitor.svg
+[npm-url]: https://npmjs.org/package/eruda-monitor
+[ci-image]: https://img.shields.io/github/actions/workflow/status/liriliri/eruda-monitor/main.yml?branch=master&style=flat-square
+[ci-url]: https://github.com/liriliri/eruda-monitor/actions/workflows/main.yml 
+[license-image]: https://img.shields.io/npm/l/eruda-monitor.svg
+
 Eruda plugin for fps and memory.
 
 ## Demo
--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-const LunaPerformanceMonitor = require('luna-performance-monitor')
 const raf = require('licia/raf')
+const LunaPerformanceMonitor = require('luna-performance-monitor')
 const now = require('licia/now')
-const contain = require('licia/contain')
 
-module.exports = function(eruda) {
+module.exports = function (eruda) {
   let { evalCss } = eruda.util
 
   class Monitor extends eruda.Tool {
     constructor() {
       super()
       this.name = 'monitor'
-      this._style = evalCss(require('./style.scss') + require('luna-performance-monitor/luna-performance-monitor.css'))
+      this._style = evalCss(
+        require('./style.scss') +
+          require('luna-performance-monitor/luna-performance-monitor.css')
+      )
     }
     init($el, container) {
       super.init($el, container)
       $el.html('<div class="eruda-fps"></div><div class="eruda-memory"></div>')
-      
+
       this._initFps($el.find('.eruda-fps').get(0))
       if (performance.memory) {
         this._initMemory($el.find('.eruda-memory').get(0))
@@ -93,7 +95,7 @@ module.exports = function(eruda) {
       })
       this._memoryMonitor = memoryMonitor
     }
-    _onThemeChange = (name, val) => {
+    _onThemeChange = (name) => {
       const fpsMonitor = this._fpsMonitor
       const memoryMonitor = this._memoryMonitor
       if (name === 'theme') {
